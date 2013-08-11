4908b73dd757d1f839dd62cd02f4ccc730e84c82
GroundskeeperWillie <wmacaulay@gmail.com>
2013-08-11 19:50:25 +1000

    altered application.rb file

--- config/application.rb
+++ config/application.rb
@@ -64,14 +64,14 @@ module Ccsite5
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+        # New to initialize precompile
+    config.assets.initialize_on_precompile = false
+    config.assets.compile = true
     # Enable @font-face
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf)
     # To precompile bootstrap rather than throught the stylesheet
     config.assets.precompile += %w( bootstrap.css )
-    # New to initialize precompile
-    config.assets.initialize_on_precompile = false
-    config.assets.compile = true
   end
 end
