5d51efca5f4975476628bc109e012ea5f564da44
GroundskeeperWillie <wmacaulay@gmail.com>
2013-08-11 20:26:15 +1000

    Just OMAs response

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,12 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  
+  # As per OMA suggestion, comment this out
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  # As per OMA suggestion, uncomment this.
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Ccsite4
