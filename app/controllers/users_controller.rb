639ee3994ef0c7fd77bd0e3f85b11def935b8519
GroundskeeperWillie <wmacaulay@gmail.com>
2013-08-18 10:38:12 +1000

    updated admin controls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
-  #before_filter :admin_user, only: [:create, :destroy]
+  before_filter :admin_user, only: [:destroy]
   
   def new
     @user = User.new
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      redirect_to(root_path) #unless current_user.admin?
+      redirect_to(root_path) unless current_user.admin?
     end
     
 end
