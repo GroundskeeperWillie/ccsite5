7c9750acd12b4c46fb42319e23236c2ef8ee41ab
GroundskeeperWillie <wmacaulay@gmail.com>
2013-08-26 10:00:30 +1000

    precompile locally

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   # gem 'guard-rspec', '1.2.1'
   # gem 'guard-spork', '1.2.0'  
   # gem 'spork', '0.9.2'
+  gem 'webrick', '~> 1.3.1'
 end
 
 # Gems used only for assets and not required
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ Ccsite4::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -28,6 +28,8 @@ Ccsite4::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  #commented this out because of ssl error 
+
   config.force_ssl = true
 
   # See everything in the log (default is :info)
@@ -66,8 +68,10 @@ Ccsite4::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   # Precompile additional assets (application.css.scss customer.css.scss)
-  config.assets.precompile += %w( application.css.scss custom.css.scss jennasue.css.scss )
-  
+  config.assets.precompile += %w( application.css custom.css.scss jennasue.css.scss)
+  # Added this from web - Precompile *all* assets, except those that start with underscore
+  #config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+
   # this didn't work
   #config.assets.manifest = Rails.root.join("public/assets")
 
